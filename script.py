ec923292f52d762da1489b9075256d0b6f12a36d
Florian <florian.bernard@ynov.com>
2019-03-07 14:40:30 +0100

    Simplificaiton code et input var

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import sys
+
 def convert_to_binary(dec):
   if dec == 0:
       bin = "0"
@@ -14,30 +16,29 @@ def get_mask(cidr):
     mask[i//8] = mask[i//8] + (1 << (7 - i % 8))
   return mask
 
-def split(adr):
+def split_address(adr):
   adr = adr.split("/")
   nbr = adr[0].split(".")
   cidr = adr[1]
+  if len(nbr) != 4 and len(cidr) != 4:
+    sys.exit("Format d'addresse incorrect !!")
   return nbr, cidr
 
-add_ip = "10.92.10.1/24"
-res = ""
+add_ip = input("Votre addresse: ")
+res_ip = ""
 res_mask = ""
 
-(ip, cidr) = split(add_ip)
+(ip, cidr) = split_address(add_ip)
 mask = get_mask(int(cidr))
+
 n = 0
 while len(mask) > n:
-  nbr = mask[n]
-  res_mask = res_mask + (convert_to_binary(int(nbr)))
+  nbr_mask = mask[n]
+  nbr_ip = ip[n]
+  res_mask = res_mask + (convert_to_binary(int(nbr_mask)))
+  res_ip = res_ip + (convert_to_binary(int(nbr_ip)))
   n += 1
 
-n = 0 
-while len(ip) > n:
-  nbr = ip[n]
-  res = res + (convert_to_binary(int(nbr)))
-  n += 1
-
-print (res)
+print (res_ip)
 print (res_mask)
 
